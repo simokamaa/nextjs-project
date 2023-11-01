64995eae66e7392ab0a3cfc3a88b01102c0e25a9
Simon Kamau <simonkamau690@gmail.com>
2023-11-01 09:41:58 +0300

    Update layout.tsx
    added radix theme

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
